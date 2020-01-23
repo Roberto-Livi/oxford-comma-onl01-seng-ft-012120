8dbcce9dc3984fd7bdcbfcf34f53637811e05dfc
Roberto Livi <robertomlivi@outlook.com>
2020-01-23 13:41:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
-  if array.count == 3
+  if array.count >= 3
   array[-1] = "and #{array[-1]}"
   str = array.join(", ")
   p str
@@ -12,3 +12,4 @@ end
 oxford_comma(["kiwi"])
 oxford_comma(["kiwi", "durian"])
 oxford_comma(["kiwi", "durian", "starfruit"])
+oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
